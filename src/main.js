5fe213952a5625a7306a4fffe36f715373d13d24
Sayak Mukhopadhyay <mukhopadhyaysayak@gmail.com>
2021-05-06 17:10:14 +0530

    Linted

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
-import Vue from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import vuetify from "./plugins/vuetify";
-import "roboto-fontface/css/roboto/roboto-fontface.css";
-import "@mdi/font/css/materialdesignicons.css";
+import Vue from 'vue'
+import App from './App.vue'
+import './registerServiceWorker'
+import vuetify from './plugins/vuetify'
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import '@mdi/font/css/materialdesignicons.css'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 new Vue({
   vuetify,
-  render: (h) => h(App),
-}).$mount("#app");
+  render: (h) => h(App)
+}).$mount('#app')

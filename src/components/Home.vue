8c36727118ef1cacfd751f236cd21e0839b9e602
Sayak Mukhopadhyay <mukhopadhyaysayak@gmail.com>
2021-05-07 19:38:34 +0530

    Created a layout that works

---
+++ src/components/Home.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="d-flex flex-column" style="height: 100%" v-resize="onResize">
+    <div class="flex-grow-1 primary d-flex justify-center" ref="top">
+      <v-text-field
+        class="align-end flex-grow-0 mx-2"
+        color="white"
+        outlined
+        :style="{ width: width + 'px' }"
+      ></v-text-field>
+      <v-text-field
+        class="align-end year-number flex-grow-0 mx-2"
+        color="white"
+        outlined
+        :style="{ width: width + 'px' }"
+      ></v-text-field>
+      <v-text-field
+        class="align-end year-number flex-grow-0 mx-2"
+        color="white"
+        outlined
+        :style="{ width: width + 'px' }"
+      ></v-text-field>
+      <v-text-field
+        class="align-end year-number flex-grow-0 mx-2"
+        color="white"
+        outlined
+        :style="{ width: width + 'px' }"
+      ></v-text-field>
+    </div>
+    <div class="flex-grow-1 secondary d-flex justify-center" ref="bottom">
+      <v-text-field
+        class="align-end year-number flex-grow-0 mx-2"
+        color="white"
+        outlined
+        :style="{ width: width + 'px' }"
+      ></v-text-field>
+      <v-text-field
+        class="align-end year-number flex-grow-0 mx-2"
+        color="white"
+        outlined
+        :style="{ width: width + 'px' }"
+      ></v-text-field>
+      <v-text-field
+        class="align-end year-number flex-grow-0 mx-2"
+        color="white"
+        outlined
+        :style="{ width: width + 'px' }"
+      ></v-text-field>
+      <v-text-field
+        class="align-end year-number flex-grow-0 mx-2"
+        color="white"
+        outlined
+        :style="{ width: width + 'px' }"
+      ></v-text-field>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data() {
+    return {
+      width: 0
+    }
+  },
+  mounted() {
+    this.onResize()
+  },
+  methods: {
+    onResize() {
+      let windowHeight = window.innerHeight / 2
+      let fontSize = windowHeight - 120
+      this.width = fontSize / 1.5
+
+      this.$refs.top.children.forEach((child) => {
+        child.children[0].children[0].style = `width: ${this.width}px !important`
+        child.children[0].children[0].children[1].children[0].style = `font-size: ${fontSize}px !important`
+      })
+
+      this.$refs.bottom.children.forEach((child) => {
+        child.children[0].children[0].style = `width: ${this.width}px !important`
+        child.children[0].children[0].children[1].children[0].style = `font-size: ${fontSize}px !important`
+      })
+    }
+  }
+}
+</script>
+
+<style>
+.v-input__control {
+  height: 100%;
+}
+
+.v-input__slot {
+  height: 100%;
+  margin-top: 8px;
+}
+
+input {
+  max-height: 100% !important;
+  text-align: center;
+}
+
+.v-text-field__details {
+  display: none !important;
+}
+</style>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib/framework'
+import colors from 'vuetify/lib/util/colors'
 
 Vue.use(Vuetify)
 
@@ -10,13 +11,24 @@ export default new Vuetify({
     },
     themes: {
       light: {
-        primary: '#ee44aa',
-        secondary: '#424242',
-        accent: '#82B1FF',
-        error: '#FF5252',
-        info: '#2196F3',
-        success: '#4CAF50',
-        warning: '#FFC107'
+        primary: colors.deepOrange.base,
+        secondary: colors.lightBlue.base,
+        accent: colors.orange.base,
+        error: colors.red.base,
+        warning: colors.yellow.base,
+        info: colors.indigo.base,
+        success: colors.green.base,
+        toolbar: colors.deepOrange.base
+      },
+      dark: {
+        primary: colors.deepOrange.base,
+        secondary: colors.lightBlue.base,
+        accent: colors.orange.base,
+        error: colors.red.base,
+        warning: colors.yellow.base,
+        info: colors.indigo.base,
+        success: colors.green.base,
+        toolbar: colors.grey.darken4
       }
     }
   }
